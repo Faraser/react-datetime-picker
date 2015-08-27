636c3c23c43d8f9981dbf8c6ad109d827be6c358
Faraser <faraser@rambler.ru>
2015-08-27 14:47:21 +0300

    refactoring

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -1,24 +1,21 @@
 var React = require('react');
 var _ = require('underscore');
-
+window._ = _;
 var cal_days_labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 var cal_months_labels = ['January', 'February', 'March', 'April',
     'May', 'June', 'July', 'August', 'September',
     'October', 'November', 'December'];
 var cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function getFirstWeek(a) {
-    var firstWeek = [];
-    if (a === 1) {
-        return firstWeek
+
+function getFirstWeek(startDay) {
+    if (startDay === 1) {
+        return []
     }
-    if (a === 0) {
+    if (startDay === 0) {
         return [0, 0, 0, 0, 0, 0]
     }
-    for (var i = 1; i < a; i++) {
-        firstWeek.push(0);
-    }
-    return firstWeek;
+    return _.map(_.range(startDay-1), function(){return 0});
 }
 
 
@@ -59,6 +56,31 @@ var Calendar = React.createClass({
             currentDate: date
         })
     },
+    renderTableHeader: function () {
+        return <tr>{cal_days_labels.map(function (item, i) {
+            return (<th key={i}>{item}</th>)
+        })}</tr>
+    },
+    renderTableBody: function (startDay, monthLength) {
+        var matrix = generateMatrix(startDay, monthLength);
+        var setDate = this.props.setDate;
+        var currentDate = this.state.currentDate;
+        return matrix.map(function (item, index) {
+            return <tr key={index}>
+                {item.map(function (el, i) {
+                    if (el === 0) {
+                        return <td className="empty" key={i}></td>
+                    }
+                    return (
+                        <DateItem
+                            item={el}
+                            setDate={setDate}
+                            curDate={currentDate}
+                            key={i}
+                            />)
+                })}</tr>
+        })
+    },
     render: function () {
         var month = this.state.currentDate.getMonth();
         var year = this.state.currentDate.getYear();
@@ -72,9 +94,7 @@ var Calendar = React.createClass({
         }
         var monthName = cal_months_labels[month];
 
-        var matrix = generateMatrix(startDay, monthLength);
-        var setDate = this.props.setDate;
-        var currentDate = this.state.currentDate;
+
         return (
             <div>
                 <Header
@@ -84,28 +104,12 @@ var Calendar = React.createClass({
                     prevMonth={this.handlePrevMonth}
                     />
                 <table className="datetime__table">
-
-                    <tr>{cal_days_labels.map(function (item, i) {
-                        return (<th key={i}>{item}</th>)
-                    })}</tr>
-                    <tr>
-                        {matrix.map(function (item, index) {
-                            return <tr key={index}>
-                                {item.map(function (el, i) {
-
-                                    if (el === 0) {
-                                        return <td className="empty" key={i}></td>
-                                    }
-                                    return (
-                                        <DateItem
-                                            item={el}
-                                            setDate={setDate}
-                                            curDate={currentDate}
-                                            key={i}
-                                            />)
-                                })}</tr>
-                        })}
-                    </tr>
+                    <thead>
+                        {this.renderTableHeader()}
+                    </thead>
+                    <tbody>
+                        {this.renderTableBody(startDay, monthLength)}
+                    </tbody>
                 </table>
             </div>
         )
